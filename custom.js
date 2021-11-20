15c3ca05f4953f9ef2507bbcbe9f3b7154a5af63
Sarah James <41126204+hdblcr@users.noreply.github.com>
2021-11-20 19:34:56 +0000

    housekeeping

--- custom.js
+++ custom.js
@@ -1,7 +1,5 @@
-// declare variables 
+// declare global variables 
 var timeout = false;
-var myNow = new Date();
-var pausedAt = 0;
 var stepsList = [5, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 250, 300, 350, 400, 450, 500];
 var func = false;
 var min = 0;
@@ -9,6 +7,7 @@ var max = 0;
 var contin = true;
 var frame = 0;
 
+//===== functions to read user input and convert to useable functions
 function convertToFloat(x) {
   var wrap = () => "\"use strict\";return parseFloat(" + x + ");";
   return new Function(wrap(x));
@@ -20,7 +19,7 @@ function myFunction(func) {
   return new Function(wrap(body));
 }
 
-
+//===== class for plotly stuff ====
 class MyPlot {
   constructor(func, steps, min, max) {
     this.func = myFunction(func);
@@ -29,15 +28,15 @@ class MyPlot {
     this.max = convertToFloat(max).call(null);
     this.name = func;
     this.width = (this.max - this.min) / this.steps;
-    this.plotX = this.plotXplease(this.steps, this.width);
-    this.plotY = this.plotYplease(this.steps, this.width);
-    this.fX = this.plotXplease(100, (this.max - this.min) / 100);
-    this.fY = this.plotYplease(100, (this.max - this.min) / 100);
+    this.plotX = this.getPlotX(this.steps, this.width);
+    this.plotY = this.getPlotY(this.steps, this.width);
+    this.fX = this.getPlotX(100, (this.max - this.min) / 100);
+    this.fY = this.getPlotY(100, (this.max - this.min) / 100);
     this.overArea = this.getOverArea();
     this.upArea = this.getUpArea();
   }
 
-  plotXplease(steps, width) {
+  getPlotX(steps, width) {
     var plot = [];
     for (let i = 0; i < (steps + 1); i++) {
       plot.push((i * width) + this.min);
@@ -45,7 +44,7 @@ class MyPlot {
     return plot;
   }
 
-  plotYplease(steps, width) {
+  getPlotY(steps, width) {
     var plot = [];
     for (let i = 0; i < (steps + 1); i++) {
       let x = (i * width) + this.min;
@@ -84,6 +83,7 @@ class MyPlot {
       name: this.name
     };
 
+    // plot over then up
     var trace2 = {
       x: this.plotX,
       y: this.plotY,
@@ -94,6 +94,7 @@ class MyPlot {
       name: "Over then Up"
     };
 
+    // plot up then over
     var trace3 = {
       x: this.plotX,
       y: this.plotY,
@@ -103,6 +104,8 @@ class MyPlot {
       mode: 'none',
       name: "Up then Over"
     };
+    
+    // collect all plots
     var data = [trace1, trace2, trace3];
     var layout = {
       title: mytitle,
@@ -112,68 +115,31 @@ class MyPlot {
       },
     };
 
-    //return {data: data, layout: layout};
-
+    // Show plot
     Plotly.newPlot('plot', data, layout);
 
     // return false to prevent refresh
     return false;
   }
 }
-/*
-function sleep(ms){
-  setTimeout(function(){return 0;}, ms);
-  return 0;
-}
-*/
-/*
-async function uploadFile(filename) {
-  const controller = new AbortController()
 
-  // 5 second timeout:
-  //const timeoutId = setTimeout(() => controller.abort(), 5000)
-
-	let response = await fetch(filename, {signal: controller.signal});
-		
-	if(response.status != 200) {
-		throw new Error("Server Error: " + response.status);
-	}
-		
-	// read response stream as text
-	let text_data = await response.text();
-  //console.log(text_data);
-	return text_data;
-}
-*/
-
+//===== functions to run animation =======
 function pause() {
+  // check that we are already running
   if(!func){
     notRunning();
     return;
   }
-  console.log("pause called line 171");
-  contin = false;
-  /*
-  let pauseTime = Date.now();
-  for (let i = 0; i < timeouts.length; i++) {
-    // clear timeouts
-    timeouts[i].clear();
-    // "complete" timeouts gone by
-    if (pauseTime > timeouts[i].timeoutTime) {
-      pausedAt = i + 1;
-    } 
-  }
-  console.log(pausedAt);*/
+  contin = false; // pause interval execution
 }
 
 function resume() {
+  // check that we are already running
   if(!func){
     notRunning();
     return;
   }
-  console.log("resume called");
-  contin = true;
-  //animate();
+  contin = true; // allow to run again
 }
 
 function notRunning(){
@@ -182,111 +148,49 @@ function notRunning(){
 }
 
 function restart() {
-  clearInterval(timeout);
-  contin = true;
-  frame = 0;
+  // check that we are already running
   if(!func){
     notRunning();
     return;
   }
-  animate();
-  /*
-  for (let i=0; i<timeouts.length; i++){
-    timeouts[i].clear();
-  }
-  contin = true;
-  pausedAt = 0;
-  animate();*/
+
+  // start from scratch by clearing interval
+  clearInterval(timeout);
+  contin = true; // allow to run again
+  frame = 0; // start with frame 0
+  animate(); // kick it off
 }
 
 function animate() {
-  console.log("animate called. pausedAt = " + pausedAt);
-  // set now time
-  myNow = Date.now();
-  let time = 0;
-  const current = pausedAt % stepsList.length;
-
-  let i=current;
-
+  // new frame each second
   timeout = setInterval(function(){
-      if(contin && frame<20*stepsList.length){
-        new MyPlot(func, stepsList[frame % stepsList.length], min, max).buildPlot();
-        frame++;
-      }
-    }, 1000);
-/*
-  for (let i = current + 1; i < 20 * stepsList.length; i++) {
-    // collect timeouts
-    if(contin){
-      timeouts.push(new MyTimeout(
-
-      // timeout id is what setTimeout returns
-      setTimeout(function () {
-        // created and build each plot
-        new MyPlot(func, stepsList[i % stepsList.length], min, max).buildPlot();
-      }, 1000 * i),
-      // timeoutTime is now + the time of the setTimeout
-      myNow + 1000 * (i-current)));
+    // if we're allowed to run and aren't in an infinite loop
+    if(contin && frame<20*stepsList.length){
+      // create and show a new plot
+      new MyPlot(func, stepsList[frame % stepsList.length], min, max).buildPlot();
+      // increase frame number
+      frame++;
     }
-  }  */
+  }, 1000);
 }
 
-
+//======= kick it off, read in user input =======
 function main() {
-
-
 
   //get function, min, and max from user
   func = document.getElementById("formFunc").value;
   min = document.getElementById("formMin").value;
   max = document.getElementById("formMax").value;
   
+  // if already running, just call restart animation instead
   if (timeout){
     restart();
     return false;
   }
-  /*
-    do {
-      setTimeout(function(){
-        new MyPlot(func, stepsList[i % stepsList.length], min, max).buildPlot();
-        clearTimeout(oldtimeout);
-      }, 1000 * i)
-      i++;
-    } while (contin);
-    */
-  /*
-  var p = 0;
-  var k = 0;
-  var now = new Date();
-  for (var index = 0; index < 10; index++) {
-    (function(idx, timeout){
-        setTimeout(function() {
-            //write your code here
-            console.log("Index: " + idx + ", printed after", (new Date() - now)/1000, " Seconds");
-
-        }, timeout, idx);
-        k += 1000;
-    })(index, k);
-}*/
-
-  // display first one
-  //const first = new MyPlot(func, stepsList[0], min, max)
-  //first.buildPlot();
 
   // call animate
   animate();
-  /*
-    
-  
-    // build data
-    for (let i=0; i<stepsList.length; i++){
-      plots.push(new MyPlot(func, stepsList[i], min, max));
-    }
-  
-    // plot data
-    for (let i=0; i<plots.length; i++){
-      setTimeout(function(){
-        plots[i].buildPlot();
-      }, 1000 * i);
-    }*/
+
+  // return false to prevent refresh
+  return false;
 }
