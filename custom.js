15e8f579de81176e672bd4c269b52dab6feae482
Sarah James <41126204+hdblcr@users.noreply.github.com>
2021-11-20 19:19:57 +0000

    Build, pause, resume, restart working. Alerts for if pressing wrong buttons before plot is built.

--- custom.js
+++ custom.js
@@ -1,66 +1,78 @@
-function convertToFloat(x) {
-  var wrap = s => '"use strict";return parseFloat(' + x + ');';
-  return new Function( wrap(x) );
-} 
+// declare variables 
+var timeout = false;
+var myNow = new Date();
+var pausedAt = 0;
+var stepsList = [5, 10, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 250, 300, 350, 400, 450, 500];
+var func = false;
+var min = 0;
+var max = 0;
+var contin = true;
+var frame = 0;
 
-function myFunction(func){
-  var body = "function( x ){ return " + func + "}";
-  var wrap = s => "{ return " + body + " };";
-  return new Function(wrap (body) );
+function convertToFloat(x) {
+  var wrap = () => "\"use strict\";return parseFloat(" + x + ");";
+  return new Function(wrap(x));
 }
 
-class MyPlot{
-  constructor(func, steps, min, max){
+function myFunction(func) {
+  var body = "function( x ){ return " + func + "}";
+  var wrap = () => "{ return " + body + " };";
+  return new Function(wrap(body));
+}
+
+
+class MyPlot {
+  constructor(func, steps, min, max) {
     this.func = myFunction(func);
     this.steps = steps;
     this.min = convertToFloat(min).call(null);
     this.max = convertToFloat(max).call(null);
     this.name = func;
-    this.width = (this.max-this.min)/this.steps;
+    this.width = (this.max - this.min) / this.steps;
     this.plotX = this.plotXplease(this.steps, this.width);
     this.plotY = this.plotYplease(this.steps, this.width);
-    this.fX = this.plotXplease(100, (this.max-this.min)/100);
-    this.fY = this.plotYplease(100, (this.max-this.min)/100);
+    this.fX = this.plotXplease(100, (this.max - this.min) / 100);
+    this.fY = this.plotYplease(100, (this.max - this.min) / 100);
     this.overArea = this.getOverArea();
     this.upArea = this.getUpArea();
   }
 
-  plotXplease(steps, width){
+  plotXplease(steps, width) {
     var plot = [];
-    for (let i=0; i<(steps+1); i++){
+    for (let i = 0; i < (steps + 1); i++) {
       plot.push((i * width) + this.min);
     }
     return plot;
   }
 
-  plotYplease(steps, width){
+  plotYplease(steps, width) {
     var plot = [];
-    for (let i=0; i<(steps+1); i++){
+    for (let i = 0; i < (steps + 1); i++) {
       let x = (i * width) + this.min;
       plot.push(this.func.call(null).call(null, x));
     }
     return plot;
   }
 
-  getOverArea(){
+  getOverArea() {
     var area = 0;
-    for (let i=0; i<this.plotY.length; i++){
+    for (let i = 0; i < this.plotY.length; i++) {
       area += this.width * this.plotY[i];
     }
     return area;
   }
 
-  getUpArea(){
+  getUpArea() {
     var area = 0;
-    for (let i=0; i<this.plotY.length-1; i++){
-      area += this.width * this.plotY[i+1];
+    for (let i = 0; i < this.plotY.length - 1; i++) {
+      area += this.width * this.plotY[i + 1];
     }
     let x = this.width * this.steps + this.min;
-    area += this.func.call(null).call(null,x) ;
+    area += this.func.call(null).call(null, this.max + this.width) * this.width;
     return area;
   }
 
-  buildPlot(){
+  buildPlot() {
 
     // Create chart title
     var mytitle = "Steps: " + this.steps + "<br />Over then up area is " + this.overArea.toPrecision(5) + "<br />Up then over area is " + this.upArea.toPrecision(5);
@@ -71,11 +83,11 @@ class MyPlot{
       y: this.fY,
       name: this.name
     };
-    
+
     var trace2 = {
       x: this.plotX,
       y: this.plotY,
-      line: {shape: 'hv'},
+      line: { shape: 'hv' },
       type: 'scatter',
       fill: 'tozeroy',
       mode: 'none',
@@ -85,7 +97,7 @@ class MyPlot{
     var trace3 = {
       x: this.plotX,
       y: this.plotY,
-      line: {shape: 'vh'},
+      line: { shape: 'vh' },
       type: 'scatter',
       fill: 'tonexty',
       mode: 'none',
@@ -101,13 +113,19 @@ class MyPlot{
     };
 
     //return {data: data, layout: layout};
-    
+
     Plotly.newPlot('plot', data, layout);
 
     // return false to prevent refresh
     return false;
   }
 }
+/*
+function sleep(ms){
+  setTimeout(function(){return 0;}, ms);
+  return 0;
+}
+*/
 /*
 async function uploadFile(filename) {
   const controller = new AbortController()
@@ -127,57 +145,148 @@ async function uploadFile(filename) {
 	return text_data;
 }
 */
-function main() {
-  //get function, min, and max from user
-  var func = document.getElementById("formFunc").value;
-  var min = document.getElementById("formMin").value;
-  var max = document.getElementById("formMax").value;
 
-  var steps = [5,10,15,20,30,40,50,60,70,80,90,100,150,200,250,300,350,400,450,500];
-
-  let plots = [];
-  let frames = [];
-  const first = new MyPlot(func, steps[0], min, max)
-
-  first.buildPlot();
+function pause() {
+  if(!func){
+    notRunning();
+    return;
+  }
+  console.log("pause called line 171");
+  contin = false;
   /*
-  .then(function() {
-    // find data
-    for (let i=1; i<steps.length; i++){
-      plots.push(new MyPlot(func, steps[i], max));
-    }
-
-    // add to frames
-    for (let i=0; i < plots.length; i++){
-      frames.push(plots[i].buildPlot());
-    }
-
-    // Add frames so we can animate them
-    Plotly.addFrames('plot', frames);
-  });
-
-
-  // animate the frames
-  Plotly.animate('plot', frames, {
-    transition: {
-      duration: 100,
-    },
-    frame: {
-      duration: 1000,
-      redraw: false
-    },
-    mode: 'afterall'
-  });
-*/
-  // build data
-  for (let i=0; i<steps.length; i++){
-    plots.push(new MyPlot(func, steps[i], min, max));
+  let pauseTime = Date.now();
+  for (let i = 0; i < timeouts.length; i++) {
+    // clear timeouts
+    timeouts[i].clear();
+    // "complete" timeouts gone by
+    if (pauseTime > timeouts[i].timeoutTime) {
+      pausedAt = i + 1;
+    } 
   }
+  console.log(pausedAt);*/
+}
 
-  // plot data
-  for (let i=0; i<plots.length; i++){
-    setTimeout(function(){
-      plots[i].buildPlot();
-    }, 1000 * i);
+function resume() {
+  if(!func){
+    notRunning();
+    return;
   }
+  console.log("resume called");
+  contin = true;
+  //animate();
+}
+
+function notRunning(){
+  alert("Press \"Build Plot\" to get started.");
+  return;
+}
+
+function restart() {
+  clearInterval(timeout);
+  contin = true;
+  frame = 0;
+  if(!func){
+    notRunning();
+    return;
+  }
+  animate();
+  /*
+  for (let i=0; i<timeouts.length; i++){
+    timeouts[i].clear();
+  }
+  contin = true;
+  pausedAt = 0;
+  animate();*/
+}
+
+function animate() {
+  console.log("animate called. pausedAt = " + pausedAt);
+  // set now time
+  myNow = Date.now();
+  let time = 0;
+  const current = pausedAt % stepsList.length;
+
+  let i=current;
+
+  timeout = setInterval(function(){
+      if(contin && frame<20*stepsList.length){
+        new MyPlot(func, stepsList[frame % stepsList.length], min, max).buildPlot();
+        frame++;
+      }
+    }, 1000);
+/*
+  for (let i = current + 1; i < 20 * stepsList.length; i++) {
+    // collect timeouts
+    if(contin){
+      timeouts.push(new MyTimeout(
+
+      // timeout id is what setTimeout returns
+      setTimeout(function () {
+        // created and build each plot
+        new MyPlot(func, stepsList[i % stepsList.length], min, max).buildPlot();
+      }, 1000 * i),
+      // timeoutTime is now + the time of the setTimeout
+      myNow + 1000 * (i-current)));
+    }
+  }  */
+}
+
+
+function main() {
+
+
+
+  //get function, min, and max from user
+  func = document.getElementById("formFunc").value;
+  min = document.getElementById("formMin").value;
+  max = document.getElementById("formMax").value;
+  
+  if (timeout){
+    restart();
+    return false;
+  }
+  /*
+    do {
+      setTimeout(function(){
+        new MyPlot(func, stepsList[i % stepsList.length], min, max).buildPlot();
+        clearTimeout(oldtimeout);
+      }, 1000 * i)
+      i++;
+    } while (contin);
+    */
+  /*
+  var p = 0;
+  var k = 0;
+  var now = new Date();
+  for (var index = 0; index < 10; index++) {
+    (function(idx, timeout){
+        setTimeout(function() {
+            //write your code here
+            console.log("Index: " + idx + ", printed after", (new Date() - now)/1000, " Seconds");
+
+        }, timeout, idx);
+        k += 1000;
+    })(index, k);
+}*/
+
+  // display first one
+  //const first = new MyPlot(func, stepsList[0], min, max)
+  //first.buildPlot();
+
+  // call animate
+  animate();
+  /*
+    
+  
+    // build data
+    for (let i=0; i<stepsList.length; i++){
+      plots.push(new MyPlot(func, stepsList[i], min, max));
+    }
+  
+    // plot data
+    for (let i=0; i<plots.length; i++){
+      setTimeout(function(){
+        plots[i].buildPlot();
+      }, 1000 * i);
+    }*/
 }
